2fb73b98aa1e3adf3bb2835388ebe802570cac28
decabeza <alberto@decabeza.es>
2020-03-06 10:57:33 +0100

    Use only deutsch language

---
+++ config/initializers/application_custom.rb
@@ -0,0 +1,7 @@
+module Consul
+  class Application < Rails::Application
+    config.i18n.default_locale = :de
+    config.i18n.available_locales = ["de"]
+    config.i18n.available_locales = ["en", "es", "fr", "gl", "it", "nl", "pt-BR"] if Rails.env.test?
+  end
+end
