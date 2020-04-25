fafa38b1e976a554ea035af7b854823ca90d6016
Julian Herrero <microweb10@gmail.com>
2020-04-25 10:43:37 +0700

    Allow other languages in development mode for testing purposes

--- config/initializers/application_custom.rb
+++ config/initializers/application_custom.rb
@@ -2,6 +2,6 @@ module Consul
   class Application < Rails::Application
     config.i18n.default_locale = :de
     config.i18n.available_locales = ["de"]
-    config.i18n.available_locales = ["en", "es", "fr", "gl", "it", "nl", "pt-BR"] if Rails.env.test?
+    config.i18n.available_locales = ["de", "en", "es", "fr", "gl", "it", "nl", "pt-BR"] if Rails.env.test? || Rails.env.development?
   end
 end
