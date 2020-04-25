fd7e02bf0897b47623911cf16151e8588becb8b3
Julian Herrero <microweb10@gmail.com>
2020-04-25 12:58:57 +0700

    Add more information to the CSV exported for budget investments

--- app/models/budget/investment/exporter.rb
+++ app/models/budget/investment/exporter.rb
@@ -16,18 +16,28 @@ class Budget::Investment::Exporter
 
     def headers
       [
-        I18n.t("admin.budget_investments.index.list.id"),
-        I18n.t("admin.budget_investments.index.list.title"),
-        I18n.t("admin.budget_investments.index.list.supports"),
-        I18n.t("admin.budget_investments.index.list.admin"),
-        I18n.t("admin.budget_investments.index.list.valuator"),
-        I18n.t("admin.budget_investments.index.list.valuation_group"),
-        I18n.t("admin.budget_investments.index.list.geozone"),
-        I18n.t("admin.budget_investments.index.list.feasibility"),
-        I18n.t("admin.budget_investments.index.list.valuation_finished"),
-        I18n.t("admin.budget_investments.index.list.selected"),
-        I18n.t("admin.budget_investments.index.list.visible_to_valuators"),
-        I18n.t("admin.budget_investments.index.list.author_username")
+        #I18n.t("admin.budget_investments.index.list.id"),
+        #I18n.t("admin.budget_investments.index.list.title"),
+        #I18n.t("admin.budget_investments.index.list.supports"),
+        #I18n.t("admin.budget_investments.index.list.admin"),
+        #I18n.t("admin.budget_investments.index.list.valuator"),
+        #I18n.t("admin.budget_investments.index.list.valuation_group"),
+        #I18n.t("admin.budget_investments.index.list.geozone"),
+        #I18n.t("admin.budget_investments.index.list.feasibility"),
+        #I18n.t("admin.budget_investments.index.list.valuation_finished"),
+        #I18n.t("admin.budget_investments.index.list.selected"),
+        #I18n.t("admin.budget_investments.index.list.visible_to_valuators"),
+        #I18n.t("admin.budget_investments.index.list.author_username")
+        "ID",
+        "Title",
+        "Summary",
+        "Description",
+        "Petition",
+        "Supports",
+        "Tags",
+        "Category",
+        "Author",
+        "Organization"
       ]
     end
 
@@ -35,16 +45,21 @@ class Budget::Investment::Exporter
       [
         investment.id.to_s,
         investment.title,
+        ActionView::Base.full_sanitizer.sanitize(investment.summary),
+        ActionView::Base.full_sanitizer.sanitize(investment.description),
+        ActionView::Base.full_sanitizer.sanitize(investment.petition),
         investment.total_votes.to_s,
-        admin(investment),
-        investment.assigned_valuators || "-",
-        investment.assigned_valuation_groups || "-",
+        investment.tags.map(&:name).join(","),
+        #admin(investment),
+        #investment.assigned_valuators || "-",
+        #investment.assigned_valuation_groups || "-",
         investment.heading.name,
-        price(investment),
-        investment.valuation_finished? ? I18n.t("shared.yes") : I18n.t("shared.no"),
-        investment.selected? ? I18n.t("shared.yes") : I18n.t("shared.no"),
-        investment.visible_to_valuators? ? I18n.t("shared.yes") : I18n.t("shared.no"),
-        investment.author.username
+        #price(investment),
+        #investment.valuation_finished? ? I18n.t("shared.yes") : I18n.t("shared.no"),
+        #investment.selected? ? I18n.t("shared.yes") : I18n.t("shared.no"),
+        #investment.visible_to_valuators? ? I18n.t("shared.yes") : I18n.t("shared.no"),
+        investment.author.username,
+        investment.organization
       ]
     end
 
